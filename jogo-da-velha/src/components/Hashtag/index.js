c07ce985859faee272f1137b0800f2db61e96836
DaniloDuarte <danilojoseduarte@gmail.com>
2023-04-13 14:39:21 -0300

    Grid estruturada de grid criada

--- jogo-da-velha/src/components/Hashtag/index.js
+++ jogo-da-velha/src/components/Hashtag/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './styles.css';
 import Card from '../../objects/Card';
 import Player from '../../objects/Player';
 
+
 const Hashtag = () => (
     <Card>
-        <ul className='hastag'>
+        <ul className='hashtag'>
             <li className='item'><Player player = "o" /></li>
             <li className='item'><Player player = "o" /></li>
             <li className='item'><Player player = "x" /></li>
